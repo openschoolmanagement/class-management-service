8db4ee1c72ac72149addda5f0cfc4ae40f93a155
Philipp <philippkostyra@gmail.com>
2018-05-26 18:23:34 +0200

    Intial Spring Boot Setup
    Copy of files from Initialzr

---
+++ src/main/java/open/schoolmanagement/class/classmanagementservice/ClassManagementServiceApplication.java
@@ -0,0 +1,12 @@
+package open.schoolmanagement.class.classmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassManagementServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/open/schoolmanagement/class/classmanagementservice/ClassManagementServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package open.schoolmanagement.class.classmanagementservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClassManagementServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
